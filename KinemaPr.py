865692e7d791cade54337043b463f697cc85416e
thomy <thomasignaciocg@gmail.com>
2023-03-11 13:52:54 -0300

    Cambio de prueba de rama

--- KinemaPr.py
+++ KinemaPr.py
@@ -2,13 +2,15 @@
 
 
 # Suma de dos números
-num1 = 5
-num2 = 3
+num1 = 10
+num2 = 10
 suma = num1 + num2
 print("La suma de", num1, "y", num2, "es", suma)
 
 # Resta de dos números
-num3 = 7
-num4 = 2
+num3 = 10
+num4 = 10
 resta = num3 - num4
 print("La resta de", num3, "y", num4, "es", resta)
+
+#ESTE CAMBIO ESTA LIGADO A RAMA THOMAS
