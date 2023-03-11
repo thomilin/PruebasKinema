1b840ddc3fd004c739db41c733d38e83a4f399c7
thomy <thomasignaciocg@gmail.com>
2023-03-11 13:31:45 -0300

    Prueba CGpt(002)

---
+++ KinemaPr.py
@@ -0,0 +1,14 @@
+# CODIGO CHATGPT DE PRUEBA
+
+
+# Suma de dos números
+num1 = 5
+num2 = 3
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es", suma)
+
+# Resta de dos números
+num3 = 7
+num4 = 2
+resta = num3 - num4
+print("La resta de", num3, "y", num4, "es", resta)
